5ba335197fd1233389c08bcf5b5a541ee9e86b42
mohamedosama2074 <iosengks74@gmail.com>
2020-01-31 17:33:30 +0200

    Facebook login

--- FacebookSDK/ViewController.swift
+++ FacebookSDK/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var facebookButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func facebookLoginButton(_ sender: UIButton) {
+        
+        let manager = LoginManager()
+        manager.logIn(permissions: [.publicProfile , .email , .userAboutMe], viewController: self) { (result) in
+            
+            switch result{
+                
+            case .success(granted: let permissionGranted, declined: let permessionDeclined, token: let userToken):
+                print("the user token is \(userToken)")
+                
+                break
+            case .cancelled:
+                
+                print("cancelled")
+                break
+                
+            case .failed(_):
+                print("faied")
+                break
+            }
+        }
+    }
 }
 
